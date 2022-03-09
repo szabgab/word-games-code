7971f08771b1ec4d33089d8fb9ed0b54bcebb8b6
Gabor Szabo <gabor@szabgab.com>
2022-03-09 20:39:15 +0100

    move todos to issues in GitHub

--- docs/js/games.js
+++ docs/js/games.js
@@ -396,32 +396,3 @@ $(document).ready(function(){
     $("#cancel_config").click(cancel_config);
     $( "html" ).keypress(real_keyboard_pressed);
 });
-
-// TODO: make the letters on the buttons bigget, but the buttons smaller, make sure they fit in the width of the screen
-
-// TODO: recognize when the data is not available (yet) or if there was a network error and let the user know.
-
-
-// TODO: Add version numer or released date to the about page.
-// TODO: If there are non-letters in the text, show them as they are
-
-// TODO Introduce In-game money;
-//    start by giving 100 coins to the user
-//    make each guess cost 1
-//    make each bad guess cost 2
-//    make each hint cost 3
-//    when the user finds a word give her money revard (based on the length of the word, the number of different letters, full size of the abc)
-//    if the user runs out of money allow her to ask for more (or buy more)
-
-// TODO: Create some images that can be easily shared by people that also include the link to the site.
-
-// TODO: Allow the user to mark specific games and languages to be available off-line. Then download them and keep them in separate variables or even in the local storage so we can switch to that language game while offline as well
-// TODO: Introduce levels: First level is short and simple words, the higher the level the longer the words or multiword expressions using more complex words. (e.g. in hungarian it might be more simple to have words that only use aeiou vowels and not the umlauted ones.)
-// TODO: Allow people to create word sets for levels and the allow others to select "game" of someone. This could be used by teachers to prepare words based on the level of the students.
-
-// TODO: shall we give some feedback when someone presses a key that is disabled on the virtual keyboard? This is only relevant on a computer, right?
-
-// TODO: Indicate language and game when we load the app
-
-// TODO: change the config page to be modal so it will go back to the same place where we started from (well, we might need to load new data and once it is loaded we should go back to the game page if that's where we came from)
-// I am not sure if we need to make it modal, but it looked strange when i clicked on it after i finished a game and then it went back to the main page.
