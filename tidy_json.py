8a66f1be1aef0896e6d1dc3fb5a48c32c4711a82
Gabor Szabo <gabor@szabgab.com>
2022-03-17 21:03:17 +0100

    allow the reuse of the funcitions

--- tidy_json.py
+++ tidy_json.py
@@ -6,7 +6,7 @@ import sys
 # format the json files
 
 def tidy(filename):
-    print(filename)
+    #print(filename)
     with open(filename) as fh:
         data = json.load(fh)
     for category in data.keys():
@@ -14,7 +14,8 @@ def tidy(filename):
     with open(filename, 'w') as fh:
         json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
 
-for filename in sys.argv[1:]:
-    tidy(filename)
+if __name__== '__main__':
+    for filename in sys.argv[1:]:
+        tidy(filename)
 
 # vim: expandtab
